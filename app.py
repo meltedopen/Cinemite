8983ed02fe645f9142428ad2ce6308a77b8f1a04
tiffanyteaze <tteaze@gmail.com>
2019-03-19 18:12:08 -0700

    adds registration form

--- app.py
+++ app.py
@@ -1,37 +1,72 @@
 from flask import Flask, g
 from flask import render_template, flash, redirect, url_for
-# This gains access to our models
-import models
-from forms import
-from forms import
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_bcrypt import check_password_hash
 
-app = Flask(__name__)
-app.secret_key = 'lkasmdf.lksajmflkalfgj.klsdnfaj'
+
+import models
+import forms
 
 DEBUG = True
 PORT = 8000
 
+app = Flask(__name__)
+app.secret_key = 'adkjfalj.adflja.dfnasdf.asd'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
 
 @app.before_request
 def before_request():
-    # connect to the DB before each request
+    """Connect to the database before each request."""
     g.db = models.DATABASE
     g.db.connect()
 
 
 @app.after_request
 def after_request(response):
-    # close the db connection after each request
+    """Close the database connection after each request."""
     g.db.close()
     return response
 
-
 @app.route('/')
-# @app.route('/movies', methods=['GET'])
-# def index():
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World'
+def index():
+    return 'hi'
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash('Yay you registered', 'success')
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+            )
+
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
 if __name__ == '__main__':
     models.initialize()
+    try:
+        models.User.create_user(
+            username='jimbo',
+            email="jim@jim.com",
+            password='password',
+            admin=True
+            )
+    except ValueError:
+        pass
+
     app.run(debug=DEBUG, port=PORT)
--- models.py
+++ models.py
@@ -1,32 +1,33 @@
 import datetime
 from peewee import *
-DATABASE = SqliteDatabase('cinemite.db')
 
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
 
-class User(Model):
+DATABASE = SqliteDatabase('twit.db')
 
-    username = CharField()
-    email = CharField()
-    password = CharField()
-    avatar = CharField()
-
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
+    
     class Meta:
         database = DATABASE
-
-
-class List(Model):
-    user = ForeignKeyField(User, backref='mylist')
-    movie = IntegerField()
-    watched = BooleanField(default=False)
-    comment = TextField()
-    timestamp = DateTimeField(default=datetime.datetime.now)
-
-    class Meta:
-        database = DATABASE
-        order_by = ('-timestamp', )
-
-
+        
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin)
+        except IntegrityError:
+            raise ValueError("User already exists")
+            
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User, List], safe=True)
+    DATABASE.create_tables([User], safe=True)
     DATABASE.close()
