a6d01bc2c570c4deed291b98e8955bb3a0f7bf45
meltedopen <isaac.chang5@gmail.com>
2019-03-20 17:07:27 -0700

    seperated moviews from layout... started styling new navbar

--- app.py
+++ app.py
@@ -43,7 +43,10 @@ def after_request(response):
 
 @app.route('/')
 def index():
+    # if current_user.is_authenticated:
     return render_template('layout.html')
+    # else:
+    #     return render_template('landing.html')
 
 
 @app.route('/movies')
