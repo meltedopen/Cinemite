999a76864de1e7654b43dac187d7766ea6e83188
tiffanyteaze <tteaze@gmail.com>
2019-03-21 12:12:40 -0700

    pasted models from github

--- models.py
+++ models.py
@@ -46,13 +46,6 @@ class List(Model):
     comment = CharField(max_length=600, default='')
     timestamp = DateTimeField(default=datetime.datetime.now)
 
-class User(UserMixin, Model):
-    username = CharField(unique=True)
-    email = CharField(unique=True)
-    password = CharField(max_length=100)
-    joined_at = DateTimeField(default=datetime.datetime.now)
-    is_admin = BooleanField(default=False)
-    
     class Meta:
         database = DATABASE
 
