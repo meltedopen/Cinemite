ce256e2e7f9d4335e263b8e1f24aeceb4c36d16a
meltedopen <isaac.chang5@gmail.com>
2019-03-22 15:22:58 -0700

    heroku deploy #1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_bcrypt import check_password_hash
 
+from flask.ext.heroku import Heroku
 
 import models
 import forms
@@ -16,6 +17,8 @@ app.secret_key = 'adkjfalj.adflja.dfnasdf.asd'
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+heroku = Heroku(app)
 
 
 @login_manager.user_loader
